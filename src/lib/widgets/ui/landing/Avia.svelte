6655e68c82ee79ab0a4eab404fa17cfc79f49daa
DarkDev <darkdev_brand@riseup.net>
2024-09-17 17:55:56 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/Avia.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<section class="section padding-tb-8">
+  <div class="container">
+    <div class="split-section">
+      <div class="split-section-content centered-content bg-dark-green">
+        <h2 class="alternative-title light">
+          Twój wybór pomoże mi odszukać własny dom.
+        </h2>
+          <p class="paragraph-3 text-[#fffcf8]">
+            Oprócz działalności twórczej, jestem w trakcie budowy własnego domu w kontenerze starej ciężarówki. Niestety, po kilku włamaniach i kradzieżach moja ciężarówka wymaga poważnej renowacji.
+          </p>
+          <p class="paragraph-3 text-[#fffcf8]">
+            Wszystkie zyski ze sprzedaży tych ręcznie robionych magnesów przeznaczam na renowację mojego kampera.
+          </p>
+          <p class="paragraph-3 text-[#fffcf8]">
+            W tej chwili jestem w trudnej sytuacji - przydałaby mi się każda pomoc i wsparcie.
+          </p>
+
+        <a href="/" class="button-2 yellow-background w-button">
+          Więcej informacji na ten temat
+        </a>
+      </div>
+      <div class="split-section-image">
+        <img src="images/avia_1.jpg" loading="lazy" sizes="(max-width: 767px) 90vw, 46vw" alt="" class="split-image">
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+
+</style>
