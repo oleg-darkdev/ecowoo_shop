50905a7c9fde0c4e5423b69a4b2227b78aa4b612
DarkDev <darkdev_brand@riseup.net>
2024-09-17 17:55:21 +0300

    intial list of categories

---
+++ src/lib/shared/data/categories.js
@@ -0,0 +1,233 @@
+const categories = [
+  // , 'trial' 'szose',  'cyclocross',
+  {
+    tags: ['kolarstwo', 'citybike', 'bikepolo', 'MTB', 'touring', 'grawel', 'triathlon', 'fatbike', 'BMX' ],
+    title: 'Rowerzysci i rowery ',
+    emoji: '🚲',
+    desc: [
+      `Świetny prezent dla miłośników wszystkich rodzajów kolarstwa i tych, którzy cenią sobie ekologiczne podróżowanie na dwóch kółkach.`
+    ],
+    img: '/images/categories/cat_cycle.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/cycle'
+  },
+  // {
+  //   tags: ['LGBTQIA+', 'miłośc', 'pride', 'kolory'],
+  //   title: 'LGBTQIA+',
+  //   emoji: '🌈',
+  //   desc: [
+  //     `Kolekcja kolorowych magnesów pamiątkowych wykonanych z drewna, ręcznie malowanych w kolorach tęczy. Każdy magnes jest nie tylko stylowym elementem, ale także wyrazem indywidualności, równości i miłości we wszystkich jej przejawach.`,
+  //   ],
+  //   img: '/images/categories/cat_.svg',
+  //   magnets: [
+
+  //   ],
+  //   id: 0,
+  //   link: '/lgbtqia'
+  // },
+  {
+    tags: ['historia', 'folklor', 'kultura', 'etniczność', 'etnos', 'narodowość', 'tradycji'],
+      title: 'Wolna Białoruś',
+    emoji: '❤️',
+    desc: [
+        `Kolekcja drewnianych magnesów inspirowanych bogatą kulturą i tradycjami Białorusi.`,
+        `Magnesy reprezentują unikalne elementy białoruskiej sztuki ludowej: od narodowych ornamentów i symboli po wizerunki zamków.`
+      ],
+    img: '/images/categories/cat_belarus.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/belarus'
+  },
+  // {
+  //   tags: ['', '', '', '', ''],
+  //   emoji: '♟️',
+  //   title: 'Szachmistrze',
+  //   desc: [
+  //     ``
+  //   ],
+  //   img: '/images/categories/cat_.svg',
+  //   magnets: [
+
+  //   ],
+  //   id: 0,
+  //   link: '/chess'
+  // },
+  {
+    // antykapitalizm
+    tags: ['A.C.A.B.', 'H.W.D.P.', 'oi!', 'anty-autorytaryzm', 'punk', 'anty-globalizm', 'kontrkultura', 'anarchizm'],
+    title: 'Anty-systemowe',
+    emoji: '🏴‍☠️',
+    desc: [
+      `Kolekcja magnesów z symbolami politycznych i nieformalnych ruchów antysystemowych.`
+    ],
+    img: '/images/categories/cat_anti-system.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/anti-system'
+  }, {
+    tags: ['', '', '', '', ''],
+    title: 'Maskotki',
+    emoji: '👻',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_films.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/heroes'
+  }, {
+    tags: ['van life', 'kemping', 'podróżowanie', 'kamper', 'przyczepa kempingowa'],
+    title: 'Kampery & Van life',
+    emoji: '🚐',
+    desc: [
+      `Kolekcja magnesów o życiu na kółkach, z atmosferą przygody, kempingu i nomadycznego stylu życia.`,
+      `Obrazy kamperów, vanów, przyczep i kamperów przypominają o nieskończonych możliwościach podróżowania, bycia jednością z naturą i dążenia do niezależności.`,
+      `Pamiatki z podróży lub inspiracją do wytyczania nowych szlaków, dekorując dom, vana lub kampera stylowym i ekologicznym dodatkiem.`,
+    ],
+    img: '/images/categories/cat_van.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/van-life'
+  }, {
+    //
+    tags: ['turystyka', 'podróże', '', 'krajobrazy', 'krajoznawstwo', 'góry',  'ekoturystyka'],
+    title: 'Turystyka & Podróżowanie',
+    emoji: '🧭',
+    desc: [
+      `Kolekcja dla tych, którzy nie wyobrażają sobie życia bez nowych odkryć i przygód. Ekologiczne pamiątki do domu lub przestrzeni roboczej, które będą przypominać o piękności świata i nowych horyzontach.`
+    ],
+    img: '/images/categories/cat_tourism.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/tourism'
+  }, {
+    tags: ['', '', '', '', ''],
+    title: 'Historyczne',
+    emoji: '🏰',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_history.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/history'
+  }, {
+    tags: ['', '', '', '', ''],
+    title: 'Zwierzaki',
+    emoji: '😼',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_animals.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/animals'
+  }, {
+    tags: ['', '', '', '', ''],
+    title: 'Kwiatki',
+    emoji: '🌺',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_flowers.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/flowers'
+  },
+//
+  {
+    tags: ['', '', '', '', ''],
+    title: 'Natura',
+    emoji: '🍀',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_nature.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/cycle'
+  },
+  {
+    tags: ['', '', '', '', ''],
+    // & Malarze
+    title: 'Twórcy & Grafficiarze',
+    emoji: '🖍️',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_art.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/graffiti'
+  },
+  {
+    tags: ['MKS', 'ISS', '', '', ''],
+    title: 'Kosmos',
+    emoji: '',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_cosmos.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/cosmos'
+  },
+  {
+    tags: ['', '', '', '', ''],
+    title: 'Dla najlepszej osoby',
+    emoji: '💕',
+    desc: [
+      ``
+    ],
+    img: '/images/categories/cat_lovers.svg',
+    magnets: [
+
+    ],
+    id: 0,
+    link: '/pair'
+  },
+  // {
+  //   tags: ['', '', '', '', ''],
+  //   title: '',
+  //   emoji: '',
+  //   desc: [
+  //     ``
+  //   ],
+  //   img: '/images/categories/cat_.svg',
+  //   magnets: [
+
+  //   ],
+  //   id: 0,
+  //   link: '/cycle'
+  // },
+
+
+
+]
+
+export default categories;
