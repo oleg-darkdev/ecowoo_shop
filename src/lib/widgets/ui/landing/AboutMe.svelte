331d154821ba0e39d75a00022eff95d908a01b57
DarkDev <darkdev_brand@riseup.net>
2024-09-17 17:56:14 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/AboutMe.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<section class="section padding-tb-8">
+  <div class="full-width-content">
+    <p class="all-caps label">About me</p>
+    <h2 class="alternative-title">
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    </h2>
+  </div>
+  <div class="container">
+    <div class="w-dyn-list">
+      <div role="list" class="w-dyn-items">
+        <div role="listitem" class="w-dyn-item">
+          <div class="split-section">
+            <div class="split-section-image left-side">
+              <img src="images/oleg.JPG" loading="lazy" alt="" class="split-image">
+            </div>
+            <div class="split-section-content centered-content right-side bg-honey">
+              <h2 class="alternative-title small">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </h2>
+              <p class="labels-text">
+                Lorem Ipsum has
+              </p>
+              <a href="/" class="button-2 yellow-background large-padding w-button">
+                Lorem Ipsum has been the industry
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
