3d4207371e966092a781a4fb6ee96766676efb51
DarkDev <darkdev_brand@riseup.net>
2024-09-17 17:57:06 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/Testimonials.svelte
@@ -0,0 +1,56 @@
+<script>
+  const testimonials = [
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]
+</script>
+
+<section class="section padding-tb4">
+  <div class="container">
+    <div class="testimonial-slider-2 w-slider" >
+
+      <div class="testimonial-mask w-slider-mask">
+        {#each testimonials as testimonial}
+        <div class="testimonial-slide-2 w-slide">
+          <div class="w-dyn-list">
+            <div role="list" class="w-dyn-items">
+              <div role="listitem" class="collection-item-3 w-dyn-item">
+                <a href="#" class="testimonial-content-wrap w-inline-block">
+                  <div class="testimonial-content">
+                    <div class="testimonial-image-wrap"></div>
+                    <blockquote class="block-quote">Naturbelassener Setpember-Vino mir einer Pizza. Whoop, Whoop!</blockquote>
+                    <div class="center">
+                      <h5 class="heading-4">Manuel, Raphael &amp; Team</h5>
+                      <div class="button-2 yellow-background cart">Da Marcello</div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/each}
+      </div>
+
+      <div class="left-arrow-2 w-slider-arrow-left">
+        <div class="arrow-icon">
+          <img src="images/6270e9a0923cfe78532e3edc_220414_Setpember_Icon_Pfeil%20links.svg" alt="" class="slider-arrow-img">
+        </div>
+      </div>
+      <div class="right-arrow-2 w-slider-arrow-right">
+        <div class="arrow-icon">
+          <img src="images/6270e9a0818daec4582f5447_220414_Setpember_Icon_Pfeil%20rechts.svg" alt="" class="slider-arrow-img">
+        </div>
+      </div>
+      <div class="slide-nav w-slider-nav w-round"></div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
