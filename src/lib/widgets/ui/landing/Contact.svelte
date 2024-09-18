64bf36548354351276d70424cc3b8a9cf9588e19
DarkDev <darkdev_brand@riseup.net>
2024-09-18 17:24:30 +0300

    upd text, add emoji & email btn

---
+++ src/lib/widgets/ui/landing/Contact.svelte
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+<section class="section padding-tb4">
+  <a name="contact"></a>
+
+  <div class="container">
+    <div class="split-section">
+      <div  class="split-ig-section-content bg-beige">
+        <div class="split-ig-title">
+          <h2 class="alternative-title">Kontakt ze mną? ☎️</h2>
+        </div>
+        <div class="big-paragraph">
+          Zawsze jestem otwarty na nowe znajomości i propozycje, jeśli masz pomysły na współtworzenie, możesz wysłać do mnie e-mail lub za pośrednictwem mediów.
+        </div>
+
+        <div class="social-icons mb-8">
+          <a href="https://www.instagram.com/jesus.hobo/" target="_blank" class="social-links w-inline-block">
+            <img src="images/instagram.svg" loading="lazy" alt="instagram icon" class="social-icon">
+          </a>
+          <a href="https://www.facebook.com/hulio.mondre" target="_blank" class="social-links w-inline-block">
+            <img src="images/facebook.svg" loading="lazy" alt="facebook icon" class="social-icon">
+          </a>
+          <a href="mailto:darkdev_brand@riseup.net" target="_blank" class="social-links w-inline-block">
+            <img src="images/email.svg" loading="lazy" alt="email icon" class="social-icon">
+          </a>
+        </div>
+
+        <div class="big-paragraph">
+          W razie jakichkolwiek pytań proszę o <a href="#faq">zwrócenie uwagi na blok F.A.Q.</a> - jeśli nie ma tam odpowiedzi, możesz śmiało pytać mnie w prywatnych wiadomościach.
+        </div>
+      </div>
+      <div class="split-ig-image-wrapper">
+        <div class="w-layout-grid split-ig-grid">
+          <div class="split-ig-image social-1">
+            <img src="images/process_2.jpg" loading="lazy" sizes="(max-width: 991px) 100vw, 45vw"  alt="" class="image-cover">
+          </div>
+          <div class="split-ig-image social-2">
+            <img src="images/61faf381c4de8095aaa51bb0_271333764_340787597628354_5016791582495773470_n.webp" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 90vw, (max-width: 991px) 46vw, 45vw"  alt="" class="image-cover">
+          </div>
+          <div   class="split-ig-image social-3">
+            <img src="images/61faf381dc38ea033df37aac_244576806_192366809686915_4848694441419075826_n.webp" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 90vw, (max-width: 991px) 46vw, 45vw" alt="" class="image-cover">
+          </div>
+          <div  class="split-ig-image social-4">
+            <img src="images/oleg.JPG" loading="lazy" sizes="(max-width: 991px) 100vw, 45vw"  alt="" class="image-cover">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<style>
+
+</style>
