69bebb177881268cdb9aebdbd6502e5714e4abd5
DarkDev <darkdev_brand@riseup.net>
2024-09-18 16:45:40 +0300

    add content

--- src/lib/shared/data/categories.js
+++ src/lib/shared/data/categories.js
@@ -1,19 +1,6 @@
 const categories = [
   // , 'trial' 'szose',  'cyclocross',
-  {
-    tags: ['kolarstwo', 'citybike', 'bikepolo', 'MTB', 'touring', 'grawel', 'triathlon', 'fatbike', 'BMX' ],
-    title: 'Rowerzysci i rowery ',
-    emoji: '🚲',
-    desc: [
-      `Świetny prezent dla miłośników wszystkich rodzajów kolarstwa i tych, którzy cenią sobie ekologiczne podróżowanie na dwóch kółkach.`
-    ],
-    img: '/images/categories/cat_cycle.svg',
-    magnets: [
 
-    ],
-    id: 0,
-    link: '/cycle'
-  },
   // {
   //   tags: ['LGBTQIA+', 'miłośc', 'pride', 'kolory'],
   //   title: 'LGBTQIA+',
@@ -25,24 +12,10 @@ const categories = [
   //   magnets: [
 
   //   ],
-  //   id: 0,
+  //   id: 0, brandColor: '',
   //   link: '/lgbtqia'
   // },
-  {
-    tags: ['historia', 'folklor', 'kultura', 'etniczność', 'etnos', 'narodowość', 'tradycji'],
-      title: 'Wolna Białoruś',
-    emoji: '❤️',
-    desc: [
-        `Kolekcja drewnianych magnesów inspirowanych bogatą kulturą i tradycjami Białorusi.`,
-        `Magnesy reprezentują unikalne elementy białoruskiej sztuki ludowej: od narodowych ornamentów i symboli po wizerunki zamków.`
-      ],
-    img: '/images/categories/cat_belarus.svg',
-    magnets: [
 
-    ],
-    id: 0,
-    link: '/belarus'
-  },
   // {
   //   tags: ['', '', '', '', ''],
   //   emoji: '♟️',
@@ -54,37 +27,122 @@ const categories = [
   //   magnets: [
 
   //   ],
-  //   id: 0,
+  //   id: 0, brandColor: '',
   //   link: '/chess'
   // },
   {
-    // antykapitalizm
-    tags: ['A.C.A.B.', 'H.W.D.P.', 'oi!', 'anty-autorytaryzm', 'punk', 'anty-globalizm', 'kontrkultura', 'anarchizm'],
-    title: 'Anty-systemowe',
-    emoji: '🏴‍☠️',
+    tags: ['serduszko', 'romans', 'sympatie', 'miłość', 'delikatność', 'dbałość', 'relacje', 'rocznica', 'dlaniego', 'dlaniej', 'wskazówka'],
+    title: 'Dla najlepszej osoby',
+    emoji: '💕',
     desc: [
-      `Kolekcja magnesów z symbolami politycznych i nieformalnych ruchów antysystemowych.`
+      `Kolekcja drewnianych magnesów pamiątkowych stworzonych z ciepłem i miłością. Te magnesy to symbole uczucia, romantyzmu i opieki, idealne do wyrażania twoich miłości.`,
+      `Ten mały, ale znaczący prezent będzie pięknym przypomnieniem twojej miłości i troski, dekorując wspólną przestrzeń lub uzupełniając ważne momenty w twoim życiu.`
     ],
-    img: '/images/categories/cat_anti-system.svg',
+    img: '/images/categories/cat_lovers.svg',
     magnets: [
 
     ],
-    id: 0,
-    link: '/anti-system'
-  }, {
-    tags: ['', '', '', '', ''],
+    id: 0, brandColor: 'lovers-bg',
+    link: '/pair'
+  },
+     {
+    tags: ['nostalgia', 'bohaterowie', 'kreskówki', 'filmy', 'postacie'],
     title: 'Maskotki',
     emoji: '👻',
     desc: [
-      ``
+      `Kolekcja drewnianych magnesów pamiątkowych z wizerunkami kultowych postaci uwielbianych przez widzów w każdym wieku.`,
+      `Każdy magnes oddaje atmosferę naszych ulubionych historii, przypominając nam o chwilach, które sprawiają, że się śmiejemy, troszczymy i cieszymy. Magnesy te są idealne dla fanów klasycznych kreskówek i popularnych filmów. `
     ],
     img: '/images/categories/cat_films.svg',
     magnets: [
 
     ],
-    id: 0,
+    id: 0, brandColor: 'films-bg',
     link: '/heroes'
-  }, {
+  },
+  {
+    tags: ['zwierzaki', 'pupile', 'koty', 'fauna', 'ptaki', 'natura', 'przyroda'],
+    title: 'Zwierzaki',
+    emoji: '😼',
+    desc: [
+      `Kolekcja drewnianych magnesów pamiątkowych z wizerunkami różnorodnych przedstawicieli świata zwierząt. Magnesy te uosabiają naturalne piękno i urok, przedstawiając dzikie i domowe zwierzęta, mieszkańców lasów, oceanów i egzotycznych zakątków naszej planety.`,
+      'Świetny prezent dla Twojego zwierzaka.'
+    ],
+    img: '/images/categories/cat_animals.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'animals-bg',
+    link: '/animals'
+  },
+  {
+    tags: ['kwiatki', 'rośliny', 'flora', 'wiosna', 'róża', 'lawenda', 'rumianek', 'irys', 'orchidea', 'chryzantema', 'dalia', 'kaktus'],
+    title: 'Kwiatki',
+    emoji: '🌺',
+    desc: [
+      `Kolekcja magnesów  inspirowanych pięknem natury i różnorodnością świata kwiatów.`,
+      `Każdy magnes ozdobiony wizerunkami ulubionych kwiatów - od delikatnych róż i polnych stokrotek po jasną lawendę i egzotyczne rośliny.`,
+      `Magnesy te wprowadzą do Twojego domu atmosferę wiosny, świeżości i naturalnej harmonii, dekorując wnętrze i nadając nastrój kwitnącego ogrodu.`,
+    ],
+    img: '/images/categories/cat_flowers.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: '',
+    link: '/flowers'
+  },
+  {
+    tags: ['planetka', 'góry', 'las', 'drzewa', 'liście',  'globus', 'eko-frendly', 'ekologia', 'ekoupominki', ],
+    title: 'Natura',
+    emoji: '🍀',
+    desc: [
+      `Kolekcja magnesów oddających atmosferę spokoju i harmonii, którą zapewnia las, zachowując ją w unikalnym drewnianym wykonaniu.`,
+      `Te pamiątki będą wspaniałym przypomnieniem piękna dzikiej przyrody i pomogą ci zachować kawałek leśnej magii w twoim domu. `,
+      `Idealny dla miłośników natury i każdego, kto ceni sobie spokój i prywatność, jakie zapewniają lasy.`,
+    ],
+    img: '/images/categories/cat_nature.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'nature-bg',
+    link: '/nature'
+  },
+  {
+    tags: ['graffiti', 'street-art', 'twórczość', 'sztuka', 'kreatywność', 'kaligrafia'],
+    // & Malarze
+    title: 'Twórcy & Grafficiarze',
+    emoji: '🖍️',
+    desc: [
+      `Kolekcja magnesów inspirowanych uliczną sztuką i twórczością artystyczną.`,
+      `Każdy magnes to małe dzieło sztuki stworzone dla tych, którzy cenią kreatywność, wyrażanie siebie i energię miejskiej kultury.`,
+      `Są idealne dla fanów graffiti i oryginalnego stylu, nadając wnętrzu ulicznych akcentów.`,
+    ],
+    img: '/images/categories/cat_art.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'graffiti-bg',
+    link: '/graffiti'
+  },
+  {
+    tags: ['kosmos',  'kosmici', 'UFO',  'astronomia', 'astro', 'uniwersum', 'planety', 'gwiazdy', 'galaktyka', 'MKS', 'ISS', 'asteroidy', 'planetoidy',],
+    title: 'Kosmos',
+    emoji: '👽',
+    desc: [
+      `Kolekcja magnesów inspirowanych tajemniczym i wielkim kosmosem.`,
+      `Magnesy te przedstawiają planety, gwiazdy, galaktyki i obce formy życia, przenosząc Cię w atmosferę fantastycznych światów i podróży kosmicznych. `,
+      `Te kosmiczne magnesy staną się stylową ozdobą Twojego wnętrza, przypominając Ci o tajemnicach wszechświata i nieskończonych możliwościach, jakie otwiera. Idealny prezent dla marzycieli i tych, którzy patrzą w gwiazdy.`,
+    ],
+    img: '/images/categories/cat_cosmos.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'cosmos-bg',
+    link: '/cosmos'
+  },
+
+
+  {
     tags: ['van life', 'kemping', 'podróżowanie', 'kamper', 'przyczepa kempingowa'],
     title: 'Kampery & Van life',
     emoji: '🚐',
@@ -97,11 +155,26 @@ const categories = [
     magnets: [
 
     ],
-    id: 0,
+    id: 0, brandColor: 'van-bg',
     link: '/van-life'
-  }, {
-    //
-    tags: ['turystyka', 'podróże', '', 'krajobrazy', 'krajoznawstwo', 'góry',  'ekoturystyka'],
+  },
+  {
+    tags: ['kolarstwo', 'citybike', 'bikepolo', 'MTB', 'touring', 'grawel', 'triathlon', 'fatbike', 'BMX' ],
+    title: 'Rowerzysci i rowery ',
+    emoji: '🚲',
+    desc: [
+      `Świetny prezent dla miłośników wszystkich rodzajów kolarstwa i tych, którzy cenią sobie ekologiczne podróżowanie na dwóch kółkach.`
+    ],
+    img: '/images/categories/cat_cycle.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'cycle-bg',
+    link: '/cycle'
+  },
+  {
+
+    tags: ['turystyka', 'podróże', 'krajobrazy', 'krajoznawstwo', 'góry',  'ekoturystyka'],
     title: 'Turystyka & Podróżowanie',
     emoji: '🧭',
     desc: [
@@ -111,105 +184,54 @@ const categories = [
     magnets: [
 
     ],
-    id: 0,
+    id: 0, brandColor: 'tourism-bg',
     link: '/tourism'
-  }, {
-    tags: ['', '', '', '', ''],
+  },
+  {
+    tags: ['historia', 'folklor', 'kultura', 'etniczność', 'etnos', 'narodowość', 'tradycji'],
+      title: 'Wolna Białoruś',
+    emoji: '❤️',
+    desc: [
+        `Kolekcja drewnianych magnesów inspirowanych bogatą kulturą i tradycjami Białorusi.`,
+        `Magnesy reprezentują unikalne elementy białoruskiej sztuki ludowej: od narodowych ornamentów i symboli po wizerunki zamków.`
+      ],
+    img: '/images/categories/cat_belarus.svg',
+    magnets: [
+
+    ],
+    id: 0, brandColor: 'belarus-bg',
+    link: '/belarus'
+  },
+     {
+    tags: ['historia', 'archeologia', 'paleontologia', 'zamki', 'twierdze', 'fortyfikacje', 'zabytki', 'rycerze', 'wikingowie', 'neolit', 'paleolit'],
     title: 'Historyczne',
     emoji: '🏰',
     desc: [
-      ``
+      `Kolekcja inspirowana fascynującymi stronami z przeszłości. Każdy magnes to mały kawałek historii.`,
+      `Magnesy te odzwierciedlają wspaniałość starożytnych zamków, twierdz i fortyfikacji, a także oddają atmosferę znalezisk archeologicznych i odkryć paleontologicznych.`,
+      `Idealne dla miłośników historii, archeologii i paleontologii, pamiątki te będą nie tylko stylową ozdobą wnętrza, ale także cennym przypomnieniem bogactwa ludzkiej cywilizacji i bogactwa naturalnego dziedzictwa.`,
     ],
     img: '/images/categories/cat_history.svg',
     magnets: [
 
     ],
-    id: 0,
+    id: 0, brandColor: 'history-bg',
     link: '/history'
-  }, {
-    tags: ['', '', '', '', ''],
-    title: 'Zwierzaki',
-    emoji: '😼',
-    desc: [
-      ``
-    ],
-    img: '/images/categories/cat_animals.svg',
-    magnets: [
-
-    ],
-    id: 0,
-    link: '/animals'
-  }, {
-    tags: ['', '', '', '', ''],
-    title: 'Kwiatki',
-    emoji: '🌺',
-    desc: [
-      ``
-    ],
-    img: '/images/categories/cat_flowers.svg',
-    magnets: [
-
-    ],
-    id: 0,
-    link: '/flowers'
-  },
-//
-  {
-    tags: ['', '', '', '', ''],
-    title: 'Natura',
-    emoji: '🍀',
-    desc: [
-      ``
-    ],
-    img: '/images/categories/cat_nature.svg',
-    magnets: [
-
-    ],
-    id: 0,
-    link: '/cycle'
   },
   {
-    tags: ['', '', '', '', ''],
-    // & Malarze
-    title: 'Twórcy & Grafficiarze',
-    emoji: '🖍️',
+    // antykapitalizm
+    tags: ['A.C.A.B.', 'H.W.D.P.', 'oi!', 'anty-autorytaryzm', 'punk', 'anty-globalizm', 'kontrkultura', 'anarchizm'],
+    title: 'Anty-systemowe',
+    emoji: '🏴‍☠️',
     desc: [
-      ``
+      `Kolekcja magnesów z symbolami politycznych i nieformalnych ruchów antysystemowych.`
     ],
-    img: '/images/categories/cat_art.svg',
+    img: '/images/categories/cat_anti-system.svg',
     magnets: [
 
     ],
-    id: 0,
-    link: '/graffiti'
-  },
-  {
-    tags: ['MKS', 'ISS', '', '', ''],
-    title: 'Kosmos',
-    emoji: '',
-    desc: [
-      ``
-    ],
-    img: '/images/categories/cat_cosmos.svg',
-    magnets: [
-
-    ],
-    id: 0,
-    link: '/cosmos'
-  },
-  {
-    tags: ['', '', '', '', ''],
-    title: 'Dla najlepszej osoby',
-    emoji: '💕',
-    desc: [
-      ``
-    ],
-    img: '/images/categories/cat_lovers.svg',
-    magnets: [
-
-    ],
-    id: 0,
-    link: '/pair'
+    id: 0, brandColor: 'acab-bg',
+    link: '/anti-system'
   },
   // {
   //   tags: ['', '', '', '', ''],
@@ -222,7 +244,7 @@ const categories = [
   //   magnets: [
 
   //   ],
-  //   id: 0,
+  //   id: 0, brandColor: '',
   //   link: '/cycle'
   // },
 
