e48e507ca0d2baf858351fd869b02e1077807d0a
DarkDev <darkdev_brand@riseup.net>
2024-09-24 14:45:26 +0300

    upd content

---
+++ src/lib/widgets/ui/landing/Banner.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<section class="main-hero-section">
+  <div class="main-hero-image">
+    <!-- <div data-poster-url="https://cdn.prod.website-files.com/61fab808e6267b10c23d3916/621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-poster-00001.jpg" data-video-urls="https://cdn.prod.website-files.com/61fab808e6267b10c23d3916/621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-transcode.mp4,https://cdn.prod.website-files.com/61fab808e6267b10c23d3916/621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" data-beta-bgvideo-upgrade="false" class="image-3 w-background-video w-background-video-atom">
+      <video id="23121e86-000e-dfde-a21e-74731287b27e-video" autoplay="" loop="" style="background-image:url(" https:="" cdn.prod.website-files.com="" 61fab808e6267b10c23d3916="" 621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-poster-00001.jpg")"="" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover">
+        <source src="media/621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-transcode.mp4" data-wf-ignore="true">
+        <source src="media/621e5672c565f101410f7c44_271632449_4661117234009886_2625881116429116332_n_5-transcode.webm" data-wf-ignore="true">
+      </video>
+    </div> -->
+    <div class="logo-overlay">
+      <img src="images/banner.svg" loading="lazy" alt="" class="logo-overlay-image">
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
---
+++ src/lib/widgets/ui/landing/BannerDivider.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<section class="section no-padding-tb-6">
+  <div class="container">
+    <div class="image-full-width">
+      <img loading="lazy" sizes="(max-width: 767px) 90vw, (max-width: 991px) 92vw, 93vw" src="images/all.jpg" alt="" class="image-cover">
+      <div class="image-overlay">
+        <a href="/" target="_blank" class="button-2 hero-button w-button">Pełna lista pamiątek ☝️</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
