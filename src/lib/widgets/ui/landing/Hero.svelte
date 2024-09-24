223e40104d61d95a9ff957f49e2e45295090198d
DarkDev <darkdev_brand@riseup.net>
2024-09-24 14:46:40 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<section class="section padding-tb-8">
+  <div class="container">
+    <div class="title-wrapper centered">
+      <div class="all-caps">
+        Lorem Ipsum has been the industry's
+      </div>
+      <h1 class="alternative-title">‍
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.        <a href="/">Pairing</a>.
+      </h1>
+      <a href="/" class="button-2 unicorn-bg w-button">
+        Lorem Ipsum has been the industry's
+      </a>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
