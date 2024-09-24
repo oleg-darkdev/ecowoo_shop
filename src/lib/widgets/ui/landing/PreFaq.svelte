e7982a20752ad8fd0eea6b0efc194b5ff51576af
DarkDev <darkdev_brand@riseup.net>
2024-09-24 14:46:43 +0300

    beta v.

---
+++ src/lib/widgets/ui/landing/PreFaq.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<section class="hero-section">
+  <div class="container">
+    <div class="title-wrapper small-width">
+      <h1>
+        Jakieś pytania, pomysły, propozycje?
+      </h1>
+      <p class="big-paragraph">
+        Możesz zadać mi je osobiście lub przeczytać blok pytań i odpowiedzi.
+      </p>
+      <a href="#contact" class="button-2 yellow-background w-button">
+        Kontakt ze mną
+      </a>
+      <a href="#faq" class="button-2 unicorn-bg w-button">
+        Najczęściej zadawane pytania.
+      </a>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
