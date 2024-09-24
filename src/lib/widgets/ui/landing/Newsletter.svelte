7ea0725f5a50ec0b68e457e315af558d32e10e91
DarkDev <darkdev_brand@riseup.net>
2024-09-24 14:46:51 +0300

    init

---
+++ src/lib/widgets/ui/landing/Newsletter.svelte
@@ -0,0 +1,25 @@
+<div class="section padding-tb4">
+  <div class="container newsletter">
+    <div class="title-wrapper centered">
+      <div class="all-caps">Abonnier unseren Newsletter</div>
+      <h2 class="heading-23">Beginne noch heute deine Setpember Reise. Abonnier jetzt unseren Newsletter.</h2>
+    </div>
+    <div class="form-block newsletter-section w-form">
+      <form id="wf-form-Footer-Newsletter-Form" name="wf-form-Footer-Newsletter-Form" data-name="Footer Newsletter Form" action="https://setpember.us10.list-manage.com/subscribe/post?u=857231acd9b2572aad17d8078&amp;id=f405cfdbe5&amp;f_id=008429e2f0" method="post" class="underlined-form newsletter-section" data-wf-page-id="637103ed71bc7382a7b4baac" data-wf-element-id="5fbd88fd-c56d-6513-2724-ee34110d01c2">
+        <div class="div-block-9">
+          <input class="newsletter-input w-input" maxlength="256" name="First-Name" data-name="First Name" placeholder="Vorname" type="text" id="First-Name">
+          <input class="newsletter-input w-input" maxlength="256" name="Last-Name" data-name="Last Name" placeholder="Nachname" type="text" id="Last-Name">
+          <input class="newsletter-input w-input" maxlength="256" name="Email" data-name="Email" placeholder="E-Mail Adresse" type="email" id="Email" required="">
+        </div>
+        <input type="submit" data-wait="paar Sekunden..." class="button-2 unicorn-bg newsletter w-button" value="Newsletter abonnieren">
+      </form>
+      <div class="form-success w-form-done">
+        <div>Danke! Deine Anmeldung ist eingegangen!</div>
+      </div>
+      <div class="form-error w-form-fail">
+        <div>Oops! Da scheint etwas nicht geklappt zu haben.</div>
+      </div>
+    </div>
+  </div>
+</div>
+
